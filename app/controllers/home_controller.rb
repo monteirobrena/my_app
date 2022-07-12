53cf1807a246fbf6540a90d8e1568d2297d3ec63
Brena Monteiro <monteirobrena@gmail.com>
2022-07-11 22:49:03 -0300

    Add send_error to Appsignal.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    Appsignal.send_error("My Error")
+    Appsignal.send_error({ error: "My formatted error.", message: "Specific content"})
     nil.object
   end
 end
